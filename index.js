5be8b0d91c8a6e012b5d031a6d01a7dbe805e0d5
alokkumar006 <136841670+alokkumar006@users.noreply.github.com>
2024-09-28 17:16:52 +0530

    update index.js

--- index.js
+++ index.js
@@ -24,6 +24,148 @@ app.get('/', (req, res) => {
   });
 
 
+app.post('/submit-form', async (req, res) => {
+    try {
+        // Create a new instance of your Mongoose model (FormData)
+        console.log(req.body);
+        
+        const newFormData = new userData({
+            name: req.body.name,
+            email: req.body.email,
+            air_quality: req.body.air_quality,
+            heat_islands: req.body.heat_islands,
+            infrastructure: req.body.infrastructure,
+            underserved: req.body.underserved,
+            improvement: req.body.improvement,
+            importance: req.body.importance,
+            latitude: req.body.latitude,
+            longitude: req.body.longitude
+        });
+
+        // Save the form data to MongoDB
+        await newFormData.save();
+
+        // Send a success response
+        res.send('Data saved successfully.');
+    } catch (error) {
+        console.error(error);
+        console.log(error);
+        
+        res.status(500).send('Error saving data.');
+    }
+});
+
+
+
+app.post('/signup', async (req,res) => {
+    try {
+        const { name, email, password } = req.body;
+    
+        if (!name || !email || !password) {
+          return res.status(400).send({
+            message: "The credentials are incomplete.",
+            success: false,
+          });
+        }
+    
+        const existingUser = await userData.findOne({ email });
+        if (existingUser) {
+          return res.status(409).send({
+            message: "User already exists.",
+            success: false,
+          });
+        }
+    
+        const hashedPassword = await bcrypt.hash(password,10);
+    
+        const newUser = new userData({
+          name,
+          email,
+          password: hashedPassword,
+        });
+    
+        await newUser.save();
+    
+        // Create and save Customer document if the role is 'Customer'
+        let newCustomer;
+       
+          newCustomer = new userData({
+            name,
+            email,
+            password
+          });
+    
+          await newCustomer.save();
+    
+        return res.status(201).send({
+          message: "User registered successfully.",
+          success: true,
+          newCustomer,
+        });
+      } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+          message: "Internal server error.",
+          success: false,
+        });
+      }
+})  
+
+app.get('/login', async (req,res)=>{
+    try {
+        const { email, password } = req.body;
+    
+        if (!email || !password) {
+          return res.status(400).send({
+            message: "The credentials are incomplete.",
+            success: false,
+          });
+        }
+    
+        const existingUser = await userData.findOne({ email });
+    
+        if (!existingUser) {
+          return res.status(401).send({
+            message: "The credentials are incorrect.",
+            success: false,
+          });
+        }
+    
+        const id = existingUser._id;
+    
+        const customer = await userData.findOne({ userID: id });
+    
+        const isPasswordValid = await bcrypt.compare(
+          password,
+          existingUser.password
+        );
+    
+        if (!isPasswordValid) {
+          return res.status(401).send({
+            message: "The credentials are incorrect.",
+            success: false,
+          });
+        }
+    
+        const { _id } = customer;
+    
+        return res.status(200).send({
+          message: "Login successful.",
+          success: true,
+          user: existingUser,
+          CustomerId: _id,
+          role: "Customer",
+        });
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({
+          message: "Internal server error.",
+          success: false,
+        });
+      }
+
+
+})
 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
